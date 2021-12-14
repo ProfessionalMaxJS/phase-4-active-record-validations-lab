d3faf011a594f68f2ac59c438b47029d10a8fc98
Max <max.edison@yahoo.com>
2021-12-14 16:32:54 -0500

    commit tree 09033ef8109bf533869358ab20a3546e63562c15

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+
+    validate :is_clickbait
+    validates :title, presence: true 
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction',' Non-Fiction']}
+    
+    def is_clickbait(title)
+        byebug
+    unless /[Won't Believe Secret Guess Top \d]/.match?(title)
+        errors.add(:title, "must be clickbait") 
+    end
+end
+
 end
